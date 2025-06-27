fed070c67d9e6caa320406f404c2d925d2c9ab0a
akshat 09867 <codingmaster902@gmail.com>
2025-06-27 10:59:49 +0530

    fixed grouping_variables error

--- dev/setup_initial_metadata.R
+++ dev/setup_initial_metadata.R
@@ -7,7 +7,6 @@ transactions_entry <- table_info(
       OutcomeName = "Revenue",
       ValueExpression = quote(price * quantity),
       AggregationMethods = list(
-        list(AggregatedName = "TotalRevenue", AggregationFunction = "sum", GroupingVariables = character(0)),
         list(AggregatedName = "TotalRevenueByCustomer", AggregationFunction = "sum", GroupingVariables = "customer_id"),
         list(AggregatedName = "TotalRevenueByProduct",  AggregationFunction = "sum", GroupingVariables = "product_id")
       )
@@ -16,7 +15,6 @@ transactions_entry <- table_info(
       OutcomeName = "TransactionCount",
       ValueExpression = 1,
       AggregationMethods = list(
-        list(AggregatedName = "TotalTransactions", AggregationFunction = "sum", GroupingVariables = character(0)),
         list(AggregatedName = "TransactionsByCustomer", AggregationFunction = "sum", GroupingVariables = "customer_id")
       )
     ),
@@ -24,7 +22,6 @@ transactions_entry <- table_info(
       OutcomeName = "UnitsSold",
       ValueExpression = quote(quantity),
       AggregationMethods = list(
-        list(AggregatedName = "TotalUnitsSold", AggregationFunction = "sum", GroupingVariables = character(0)),
         list(AggregatedName = "UnitsSoldByProduct", AggregationFunction = "sum", GroupingVariables = "product_id")
       )
     )
@@ -33,93 +30,38 @@ transactions_entry <- table_info(
 
 
 customers_entry <- table_info(
-  table_name = 'customers',
-  source_identifier = 'customers.csv',
-  identifier_columns = 'customer_id',
+  table_name = "customers",
+  source_identifier = "customers.csv",
+  identifier_columns = "customer_id",
   key_outcome_specs = list(
-    list(
-      OutcomeName = "CustomerDemographics",
-      ValueExpression = quote(age),
-      AggregationMethods = list(
-        list(
-          AggregatedName = "AverageAge",
-          AggregationFunction = "mean",
-          GroupingVariables = character(0)
-          )
-          )
-    )
+    list(OutcomeName = "CustomerCount", ValueExpression = 1, AggregationMethods = list(
+      list(AggregatedName = "CountByRegion", AggregationFunction = "sum", GroupingVariables = "region")
+    ))
   )
 )
-
 
 product_entry <- table_info(
-  table_name = 'products',
-  source_identifier = 'products.csv',
-  identifier_columns = 'product_id',
+  table_name = "products",
+  source_identifier = "products.csv",
+  identifier_columns = "product_id",
   key_outcome_specs = list(
-  list(
-    OutcomeName = "Averageprice",
-    ValueExpression = quote(original_price),
-    AggregationMethods = list(
-  list(
-    AggregatedName = "AverageoriginalPrice",
-    AggregationFunction = "mean",
-    GroupingVariables = character(0)
-  ),
-  list(
-    AggregatedName = "AverageoriginalPriceByCategory",
-    AggregationFunction = "mean",
-    GroupingVariables = "category"
+    list(OutcomeName = "ProductCount", ValueExpression = 1, AggregationMethods = list(
+      list(AggregatedName = "ProductsPerCategory", AggregationFunction = "sum", GroupingVariables = "category")
+    ))
   )
-)
-)
-)
 )
 
 
 
 views_entry <- table_info(
-  table_name = 'product_views',
-  source_identifier = 'views.csv',
-  identifier_columns = c('customer_id','product_id','time'),
+  table_name = "views",
+  source_identifier = "views.csv",
+  identifier_columns = c("customer_id", "product_id", "time"),
   key_outcome_specs = list(
-    list(
-      OutcomeName = "ViewEvent",
-      ValueExpression = 1,
-      AggregationMethods = list(
-        list(
-          AggregatedName = "TotalProductViews",
-          AggregationFunction = "sum",
-          GroupingVariables = character(0)
-        ),
-        list(
-          AggregatedName = "productViewsByProduct",
-          AggregationFunction = "sum", 
-          GroupingVariables = "product_id"
-        ),
-        list(
-          AggregatedName = "productViewsByCustomer",
-          AggregationFunction = "sum", 
-          GroupingVariables = "customer_id"
-        )
-      )
-    ),
-    list(
-      OutcomeName = "uniqueProductViewed", 
-      ValueExpression = quote(product_id),
-      AggregationMethods = list(
-        list(
-          AggregatedName = "totaluniqueProductViewed",
-          AggregationFunction = "n_distinct",
-          GroupingVariables = character(0)
-        ),
-        list(
-          AggregatedName = "uniqueProductViewedByCustomer",
-          AggregationFunction = "n_distinct",
-          GroupingVariables = "customer_id"
-        )
-      )
-    )
+    list(OutcomeName = "ViewCount", ValueExpression = 1, AggregationMethods = list(
+        list(AggregatedName = "ViewsByProduct", AggregationFunction = "sum", GroupingVariables = "product_id"),
+        list(AggregatedName = "ViewsByCustomer", AggregationFunction = "sum", GroupingVariables = "customer_id")
+        ))
   )
 )
 
